537affa7ecef2940c75e321b155952163cffe948
Neutron_369 <79660502+T-hawan@users.noreply.github.com>
2022-11-28 04:59:41 -0300

    Nova Funcionalidade do encomendar
    Ainda precisa melhorar, entretanto já é funcional rs.

--- js/uloadoimg.js
+++ js/uloadoimg.js
@@ -1,75 +1,94 @@
-jQuery(document).ready(function($) {
-    $('.upload-area').on('dragover', function(event){
-        event.preventDefault();
-        event.stopPropagation();
-        $(this).addClass('upload-area-hover');
-    })
-    $('.upload-area').on('dragleave', function(event){
-        event.preventDefault();
-        event.stopPropagation();
-        $(this).removeClass('upload-area-hover');
-    })
-    $('.upload-area').on('drop', function(event){
-        event.preventDefault();
-        event.stopPropagation();
-        $(this).removeClass('upload-area-hover');
-        var data = new FormData();
-        var files = event.originalEvent.dataTransfer.files;
-        for(var i=0; i<files.length; i++){
-            data.append('file[]',files[i]);
-        }
-      
-        $.ajax({
-            url:'../php/upload.php',
-            method:'POST',
-            data: data,
-            contentType: false,
-            cache: false,
-            processData: false,
-            success:function(result){
-                $('#uploaded-files').html(result);
-            }
-        })
-    })
-    $('#file').on('load',  function(event){
-        event.preventDefault();
-        event.stopPropagation();
-        $(this).removeClass('upload-area-hover');
-        var data = new FormData();
-        var files = event.originalEvent.dataTransfer.files;
-        var otfile = document.getElementById('file')
-        for(var i=0; i<files.length; i++){
-            data.append('file[]',files[i]);
-        }
-      
-        $.ajax({
-            url:'../php/upload.php',
-            method:'POST',
-            data: data,
-            contentType: false,
-            cache: false,
-            processData: false,
-            success:function(result){
-                $('#blah').html(result);
-            }
-        })
-    })
-    
-})
+//selecting all required elements
+const dropArea = document.querySelector(".drag-area"),
+dragText = dropArea.querySelector("header"),
+button = dropArea.querySelector("button"),
+input = dropArea.querySelector("input");
+let file; //this is a global variable and we'll use it inside multiple functions
+var fileobj;
 
-function leUrl(input) {
-    if (input.files && input.files[0]) { //verifica se o arquivo não está nulo
-        var reader = new FileReader(); //instancia um objeto FileReader que permite aplicações web ler o conteúdo dos arquivos (ou buffers de dados puros)
 
-        reader.onload = function (e) { //Este evento é chamado cada vez que a operação de leitura é completada com sucesso.
-            $('#blah').attr('src', e.target.result); //aqui seto a imagem no src da div a cima
-        }
-
-        reader.readAsDataURL(input.files[0]); //Inicia a leitura do conteúdo que caira após o peração completar na função a cima
-    }
+button.onclick = ()=>{
+  input.click(); //if user click on the button then the input also clicked
+  file_browse();
 }
 
-$("#file").change(function () { //aqui seto a função no evento de change do campo
-    leUrl(this);
+input.addEventListener("change", function(){
+  //getting user select file and [0] this means if user select multiple files then we'll select only the first one
+  file = this.files[0];
+  dropArea.classList.add("active");
+  showFile(); //calling function
 });
-//img pronta
+
+
+//If user Drag File Over DropArea
+dropArea.addEventListener("dragover", (event)=>{
+  event.preventDefault(); //preventing from default behaviour
+  dropArea.classList.add("active");
+  dragText.textContent = "Release to Upload File";
+});
+
+//If user leave dragged File from DropArea
+dropArea.addEventListener("dragleave", ()=>{
+  dropArea.classList.remove("active");
+  dragText.textContent = "Drag & Drop to Upload File";
+});
+
+//If user drop File on DropArea
+dropArea.addEventListener("drop", (event)=>{
+  event.preventDefault(); //preventing from default behaviour
+  //getting user select file and [0] this means if user select multiple files then we'll select only the first one
+  file = event.dataTransfer.files[0];
+  showFile(); //calling function
+});
+
+function showFile(){
+  let fileType = file.type; //getting selected file type
+  let validExtensions = ["image/jpeg", "image/jpg", "image/png"]; //adding some valid image extensions in array
+  if(validExtensions.includes(fileType)){ //if user selected file is an image file
+    let fileReader = new FileReader(); //creating new FileReader object
+    fileReader.onload = ()=>{
+      let fileURL = fileReader.result; //passing user file source in fileURL variable
+      let imgTag = `<img src="${fileURL}" alt="">`; //creating an img tag and passing user selected file source inside src attribute
+      dropArea.innerHTML = imgTag; //adding that created img tag inside dropArea container
+    }
+    fileReader.readAsDataURL(file);
+  }else{
+    alert("This is not an Image File!");
+    dropArea.classList.remove("active");
+    dragText.textContent = "Drag & Drop to Upload File";
+  }
+}
+
+
+function upload_file(e) {
+    e.preventDefault();
+    fileobj = e.dataTransfer.files[0];
+    js_file_upload(fileobj);
+}
+
+function file_browse() {
+  document.getElementById('file').onchange = function() {
+      fileobj = document.getElementById('file').files[0];
+      js_file_upload(fileobj);
+  };
+}
+
+
+function js_file_upload(file_obj) {
+    if(file_obj != undefined) {
+        var form_data = new FormData();                  
+        form_data.append('file', file_obj);
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("POST", "../php/upload.php", true);
+        xhttp.onload = function(event) {
+           
+            if (xhttp.status == 200) {
+                console.log("Uploaded!");
+            } else {
+               alert(xhttp.status);
+            }
+        }
+ 
+        xhttp.send(form_data);
+    }
+}
