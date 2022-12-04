d748913199ba619124b49ee111b6535476d4c220
devmim26 <yasmin.boaventura2004@gmail.com>
2022-12-04 06:13:44 -0300

    banco hospedado comentado

--- php/conn_banco.php
+++ php/conn_banco.php
@@ -3,10 +3,28 @@
     $banco = "luh_mimus";
     $user = "root";
     $senha = "";
+
      
     $sql = new mysqli($hostname, $user, $senha, $banco);
      
     if ($sql->connect_errno){
         echo"Falha ao conectar (" . $sql->connect_errno . ")" . $sql->connect_errno;
     }
+
+     /* //conectar banco hospedado nuvem
+
+     $hostname = "luhmimus.mysql.dbaas.com.br";
+     $banco = "luhmimus";
+     $user = "luhmimus";
+     $senha = "Luhmimus@2022";
+ 
+      
+     $sql = new mysqli($hostname, $user, $senha, $banco);
+      
+     if ($sql->connect_errno){
+         echo"Falha ao conectar (" . $sql->connect_errno . ")" . $sql->connect_errno;
+     } */
+
+
+    
 ?>
