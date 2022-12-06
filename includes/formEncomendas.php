75c31bb2a5ad9d7c4c50348668109c0901daf386
Melissa Neves <116177595+nevesmelissa@users.noreply.github.com>
2022-12-06 20:31:50 -0300

    encomendar

--- includes/formEncomendas.php
+++ includes/formEncomendas.php
@@ -1,8 +1,6 @@
-<h1 style="text-align: center; color: #ffb8c4;">Encomenda - Passo à Passo</h1>
+<h1 style="text-align: center; color: black;">Encomenda - Passo à Passo</h1>
 
-<form class="form-tot" method="POST" enctype="multipart/form-data" id="addevent">
-
-  <form action="../php/encomendar.php" method="POST">
+<form class="form-tot" method="POST" enctype="multipart/form-data" id="addevent" action="../php/encomendar.php">
     <div class="container px-1 px-md-5 px-lg-1 px-xl-5 py-5 mx-auto">
       <div class="card card0 border-0">
         <div class="row d-flex">
@@ -19,6 +17,8 @@
               </iframe>
             </div>
           </div>
+
+
           <div class="col-lg-6">
             <div class="card21 card border-0 px-4 py-5" id="forcarBaixo">
 
@@ -29,32 +29,32 @@
 
                 <div class="formbold-radio-flex">
                   <div class="formbold-radio-group">
-                    <label class="formbold-radio-label">
-                      <input class="formbold-input-radio" type="radio" name="peso" id="qusOne" />
+                    <label class="formbold-radio-label" style="font-size: 15px;">
+                      <input class="formbold-input-radio" type="radio" name="peso" id="1" value="1">
                       1KG
                       <span class="formbold-radio-checkmark"></span>
                     </label>
                   </div>
 
                   <div class="formbold-radio-group">
-                    <label class="formbold-radio-label">
-                      <input class="formbold-input-radio" type="radio" name="peso" id="qusOne" />
+                    <label class="formbold-radio-label" style="font-size: 15px;">
+                      <input class="formbold-input-radio" type="radio" name="peso" id="2" value="2">
                       2KG
                       <span class="formbold-radio-checkmark"></span>
                     </label>
                   </div>
 
                   <div class="formbold-radio-group">
-                    <label class="formbold-radio-label">
-                      <input class="formbold-input-radio" type="radio" name="peso" id="qusOne" />
+                    <label class="formbold-radio-label" style="font-size: 15px;">
+                      <input class="formbold-input-radio" type="radio" name="peso" id="3" value="3">
                       3KG
                       <span class="formbold-radio-checkmark"></span>
                     </label>
                   </div>
 
                   <div class="formbold-radio-group">
-                    <label class="formbold-radio-label">
-                      <input class="formbold-input-radio" type="radio" name="peso" id="qusOne" />
+                    <label class="formbold-radio-label" style="font-size: 15px;">
+                      <input class="formbold-input-radio" type="radio" name="peso" id="4" value="4">
                       4Kg
                       <span class="formbold-radio-checkmark"></span>
                     </label>
@@ -62,30 +62,17 @@
                 </div>
               </div>
               <div class="row px-3">
-                <label for="qusTwo" class="formbold-form-label" style="font-size: 20px; color: black;">
+                <label for="qusTwo" class="formbold-form-label" style="font-size: 20px; color: black; margin-bottom: 0px;">
                   2º Passo - Massa
                 </label>
-
-                <div class="formbold-radio-flex">
-                  <div class="formbold-radio-group">
-                    <label class="formbold-radio-label">
-                      <input class="formbold-input-radio" type="radio" name="qusTwo" id="qusTwo" />
-                      Chocolate
-                      <span class="formbold-radio-checkmark"></span>
-                    </label>
-                  </div>
-
-                  <div class="formbold-radio-group">
-                    <label class="formbold-radio-label">
-                      <input class="formbold-input-radio" type="radio" name="massa" id="qusTwo" />
-                      Baunilha
-                      <span class="formbold-radio-checkmark"></span>
-                    </label>
-                  </div>
-                </div>
-
-              </div>
-            
+                <select name="massa" id="massa" style="margin: 10px;"> 
+                  <option value="baunilha">Baunilha (massa branca)</option>
+                  <option value="chocolate">Chocolate</option>
+                  <option value="morango">Morango</option>
+                </select>
+                
+              
+          
               <div class="row px-3">
                 <label class="formbold-form-label-2" for="messageTema" style="font-size: 20px; color: black;">
                   4º Passo - Tema
@@ -99,51 +86,25 @@
                   <br>
                   <label for="file">Procurar Arquivo</label>
                   <button hidden>Browse File</button>
-                  <input type="file" name="file" id="file" hidden accesskey="i">
+                  <input type="file" name="img" id="file" >
 
                 </div>
                 <br>
-                <textarea rows="6" name="message" id="messageReche" class="formbold-form-input" placeholder="Descreva o Tema"></textarea>
+                <label class="formbold-form-label-2" for="messageTema" style="font-size: 15px; color: black;">
+                  Descreva o tema:
+                </label>
+                <textarea rows="6" name="tema" id="messageReche" class="formbold-form-input" placeholder="Ex: quero o bolo igual ao das meninas super poderosas"></textarea>
                 <script src="../js/uloadoimg.js"></script>
               </div>
-              <br>
-              <div class="row px-3">
-                <label for="opcOne" class="formbold-form-label" style="font-size: 20px; color: black;">
-                  5º Passo - Adicionais
-                </label>
-
-                <div class="formbold-radio-flex" style="display: block;">
-                  <div class="formbold-radio-group">
-                    <label class="formbold-radio-label">
-                      <input class="formbold-input-radio" type="checkbox" name="add" id="opcOne" />
-                      Chantininho - Chantilly Leite Ninho
-                      <span class="formbold-radio-checkmark"></span>
-                    </label>
-                  </div>
-
-                  <div class="formbold-radio-group" style="display: block;">
-                    <label class="formbold-radio-label">
-                      <input class="formbold-input-radio" type="checkbox" name="add" id="opcTwo" />
-                      Bolo no acetato
-                      <span class="formbold-radio-checkmark"></span>
-                    </label>
-                  </div>
-
-                  <div class="formbold-radio-group" style="display: block;">
-                    <label class="formbold-radio-label">
-                      <input class="formbold-input-radio" type="checkbox" name="add" id="opcThree" />
-                      Ganache
-                      <span class="formbold-radio-checkmark"></span>
-                    </label>
-                  </div>
-                </div>
-              </div>
+              <br><br>
+            
               <div class="row mb-3 px-3">
-                <button type="submit" class="formbold-btn add-cart" id="CadEvent">Prosseguir</button>
+                <button type="submit" class="formbold-btn add-cart" id="CadEvent" style="margin-top: 10px;">Prosseguir</button>
               </div>
 
             </div>
           </div>
+          </form>
 
         </div>
 
@@ -158,4 +119,3 @@
           </div>
         </div>
       </div>
-  </form>
--- php/encomendar.php
+++ php/encomendar.php
@@ -72,27 +72,26 @@ include 'conn_banco.php';
 $peso = $_POST['peso'];
 $massa = $_POST['massa'];
 $tema = $_POST['tema'];
-$img = $_FILES['referencia']['name'];
-$uploaddir = "..\img_encomendas";
+$img = $_FILES['img']['name'];
+$uploaddir = "img_encomendas/";
 $separa = explode(".", $img);
 $separa = array_reverse($separa);
 $tipoa = $separa[0];
 $imgrf = $peso . '.' . $tipoa;
-$add = $_POST['add'];
 
-$testar = $sql->query("SELECT * FROM encomenda WHERE qtd_encomenda = '$peso'");
+$testar = $sql->query("SELECT * FROM encomenda WHERE massa_encomendas = '$massa'");
 $check = mysqli_num_rows($testar);
 
 if ($check == 1) {
   header('location: ../pagamento/checkout.php');
 } else {
-  move_uploaded_file($_FILES["referencia"]["tmp_name"], "../" . $uploaddir .
+  move_uploaded_file($_FILES["img"]["tmp_name"], "../" . $uploaddir .
     $imgrf);
 
   $imgbd = $uploaddir . $imgrf;
 
-  $sql->query("INSERT INTO encomendas(id_encomenda, id_anuncios, qtd_encomenda, massa_encomendas, tema_encomendas, img_encomenda) VALUES
-        (NULL, NULL,'$peso', '$massa', '$tema','$imgbd')");
+  $sql->query("INSERT INTO encomenda(id_encomenda, id_anuncios, id_eventos, qtd_encomenda, massa_encomendas, tema_encomenda, img_encomenda) VALUES
+        (NULL, NULL, NULL, '$peso', '$massa', '$tema','$imgbd')");
 
   echo "Salvo";
   echo $peso;
