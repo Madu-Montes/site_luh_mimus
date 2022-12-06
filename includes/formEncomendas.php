ca31ae3eb12c6031324b328cd7ce7526ff6c6f9b
Neutron_369 <79660502+T-hawan@users.noreply.github.com>
2022-12-06 00:06:53 -0300

    ....

--- includes/formEncomendas.php
+++ includes/formEncomendas.php
@@ -1,48 +1,226 @@
+<h1 style="text-align: center; color: #ffb8c4;">Encomenda - Passo à Passo</h1>
 
-<form class="forms-tot" method="POST" enctype="multipart/form-data" id="addevent">
-        
-        <label for="qusOne" class="formbold-form-label">Peso:</label>
-        <input type="number" name="peso" id="qtd_encomendas">
+<form class="form-tot" method="POST" enctype="multipart/form-data" id="addevent">
 
-        <br>
-          
-        <div class="mb-6 pt-4">
-            <label class="formbold-form-label-2" for="messageTema" style="font-size: 30px; color: #ffb8c4;">
-                Tema:
-            </label>
-            <script src="../js/uloadoimg.js"></script>
-        </div>
-        
-        <textarea
+<form action="../php/salvar.php" method="POST">
+    <div class="container px-1 px-md-5 px-lg-1 px-xl-5 py-5 mx-auto">
+        <div class="card card0 border-0">
+            <div class="row d-flex">
+                <div class="col-lg-6">
+                    <div class="card1 pb-5">
+                        <div class="row" style="text-align: center;">
+                            <img src="../images/logo da luh.png" class="logo" style="text-align: center;">
+                        </div>
+                        <label for="messageReche" class="formbold-form-label" style="text-align: center;">
+                        Data para entregar a encomenda
+                        </label>
+
+                        <div class="scroll-escondido">
+    <iframe src="../includes/calendario/calendario.php" name="janela" id="frame-spec"></iframe>
+</div>
+    
+            </iframe>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="card21 card border-0 px-4 py-5" id="forcarBaixo">
+                        
+                        <div class="row px-3">
+                        <label for="qusOne" class="formbold-form-label">
+            1º Passo - Quantidade
+          </label>
+  
+          <div class="formbold-radio-flex">
+            <div class="formbold-radio-group">
+              <label class="formbold-radio-label">
+                <input
+                  class="formbold-input-radio"
+                  type="radio"
+                  name="qusOne"
+                  id="qusOne"
+                />
+                1KG
+                <span class="formbold-radio-checkmark"></span>
+              </label>
+            </div>
+  
+            <div class="formbold-radio-group">
+              <label class="formbold-radio-label">
+                <input
+                  class="formbold-input-radio"
+                  type="radio"
+                  name="qusOne"
+                  id="qusOne"
+                />
+                2KG
+                <span class="formbold-radio-checkmark"></span>
+              </label>
+            </div>
+  
+            <div class="formbold-radio-group">
+              <label class="formbold-radio-label">
+                <input
+                  class="formbold-input-radio"
+                  type="radio"
+                  name="qusOne"
+                  id="qusOne"
+                />
+                3KG
+                <span class="formbold-radio-checkmark"></span>
+              </label>
+            </div>
+  
+            <div class="formbold-radio-group">
+              <label class="formbold-radio-label">
+                <input
+                  class="formbold-input-radio"
+                  type="radio"
+                  name="qusOne"
+                  id="qusOne"
+                />
+                4Kg
+                <span class="formbold-radio-checkmark"></span>
+              </label>
+            </div>
+          </div>
+                        </div>
+                        <div class="row px-3">
+                        <label for="qusTwo" class="formbold-form-label">
+            2º Passo - Massa
+          </label>
+  
+          <div class="formbold-radio-flex">
+            <div class="formbold-radio-group">
+              <label class="formbold-radio-label">
+                <input
+                  class="formbold-input-radio"
+                  type="radio"
+                  name="qusTwo"
+                  id="qusTwo"
+                />
+                Chocolate
+                <span class="formbold-radio-checkmark"></span>
+              </label>
+            </div>
+  
+            <div class="formbold-radio-group">
+              <label class="formbold-radio-label">
+                <input
+                  class="formbold-input-radio"
+                  type="radio"
+                  name="qusTwo"
+                  id="qusTwo"/>
+                Baunilha
+                <span class="formbold-radio-checkmark"></span>
+              </label>
+            </div>
+          </div>
+
+                        </div>
+                        <div class="row px-3">
+                        <label for="messageReche" class="formbold-form-label">
+            3º Passo - Recheio
+          </label>
+
+          <textarea
           rows="6"
-          name="tema"
-          id="messageTema"
-          class="formbold-form-input" placeholder="Descreva o Tema Para Decorar o Bolo"
+          name="message"
+          id="messageReche"
+          class="formbold-form-input" placeholder="Descreva o Recheio"
         ></textarea>
-        <br>
-        
-        <label class="formbold-form-label-2" for="messageTema" style="font-size: 30px; color: #ffb8c4;">
-                Mande apenas uma imagem de referência:
-        </label>
-        <div class="drag-area" ondrop="upload_file(event)" ondragover="return false">
+
+                        </div>
+                        <div class="row px-3">
+                        <label class="formbold-form-label-2" for="messageTema" style="font-size: 30px; color: #ffb8c4;">
+              4º Passo - Tema
+            </label>
+    
+            <div class="drag-area" ondrop="upload_file(event)" ondragover="return false" style="margin-bottom: 20px;">
               
               <div class="icon"><i class="fas fa-cloud-upload-alt"></i></div>
               <header>Arrastar Arquivo</header>
               <span>ou</span>
               <br>
               <label for="file">Procurar Arquivo</label>
-              <input type="file" name="referencia" id="file" hidden accesskey="i">
+              <button hidden>Browse File</button>
+              <input type="file" name="file" id="file" hidden accesskey="i">
+              
               </div>
-        <br>
-        <div class="mb-6 pt-4">
-            <label class="formbold-form-label-2" style="font-size: 30px; color: #ffb8c4;">
-                Selecione uma data:
-            </label>
-            <br>
-
-            <iframe src="../includes/calendario/calendario.php" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="total"></iframe>
-
-        <button type="submit" class="formbold-btn add-cart" id="CadEvent">Prosseguir</button>
-    </form>
-    
+                <br>
+                <textarea
+          rows="6"
+          name="message"
+          id="messageReche"
+          class="formbold-form-input" placeholder="Descreva o Tema"
+        ></textarea>
+            <script src="../js/uloadoimg.js"></script>
+                        </div>
+                        <br>
+                        <div class="row px-3">
+                        <label for="opcOne" class="formbold-form-label">
+            5º Passo - Adicionais
+          </label>
+  
+          <div class="formbold-radio-flex" style="display: block;">
+            <div class="formbold-radio-group">
+              <label class="formbold-radio-label">
+                <input
+                  class="formbold-input-radio"
+                  type="checkbox"
+                  name="add"
+                  id="opcOne"
+                />
+                Chantininho - Chantilly Leite Ninho
+                <span class="formbold-radio-checkmark"></span>
+              </label>
+            </div>
+  
+            <div class="formbold-radio-group" style="display: block;">
+              <label class="formbold-radio-label">
+                <input
+                  class="formbold-input-radio"
+                  type="checkbox"
+                  name="add"
+                  id="opcTwo"
+                />
+                Bolo no acetato
+                <span class="formbold-radio-checkmark"></span>
+              </label>
+            </div>
+  
+            <div class="formbold-radio-group" style="display: block;">
+              <label class="formbold-radio-label">
+                <input
+                  class="formbold-input-radio"
+                  type="checkbox"
+                  name="add"
+                  id="opcThree"
+                />
+                Ganache
+                <span class="formbold-radio-checkmark"></span>
+              </label>
+            </div>
+          </div>
+                        </div>
+                        <div class="row mb-3 px-3">
+                        <button type="submit" class="formbold-btn add-cart" id="CadEvent">Prosseguir</button>
+                        </div>
+                        
+                    </div>
+                </div>
+            
+            </div>
+           
+            <div class="bg-blue py-4">
+                <div class="row px-3">
+                    <small class="ml-4 ml-sm-5 mb-2">Copyright &copy; 2022. Luh Mimu’s.</small>
+                    <div class="social-contact ml-4 ml-sm-auto">
+                        <span class="fa fa-facebook mr-4 text-sm"></span>
+                        <span class="fa fa-instagram mr-4 text-sm"></span>
+                        <span class="fa fa-whatsapp mr-4 mr-sm-5 text-sm"></span>
+                    </div>
+                </div>
+            </div>
+    </div> 
+</form>
     
