68cfd4db737c04e9c154c1d9cd99ea278d207235
Neutron_369 <79660502+T-hawan@users.noreply.github.com>
2022-12-06 23:50:41 -0300

    aatuallização tornando o calendário uma  etappa:)

--- includes/calendario/calendario.php
+++ includes/calendario/calendario.php
@@ -24,15 +24,90 @@ session_start();
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
         <script src="js/personalizado.js"></script>
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/carousel/">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;400&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+  <link rel="stylesheet" href="../../css/style.css">
+  <link rel="stylesheet" href="../../css/responsive.css">
+  <script defer src="../../js/app.js"></script>
     </head>
     <body>
+
+
+
+        <?php  
+        include '../headerlogged.php';
+        ?>
+        <br>
+        <div class="header-menu">
+      <nav class="navbar navbar-expand-sm navbar-light ">
+        <div class="container">
+
+          <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse"
+            data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="collapsibleNavId">
+            <ul class="navbar-nav me-auto mt-2 mt-lg-0 snip1143">
+              <li class="nav-item">
+                <a class="nav-link active" href="index.html" aria-current="page" data-hover="Home">Home<span
+                    class="visually-hidden">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="./html/galeria.html" aria-current="page"
+                  data-hover="Galeria">Galeria<span class="visually-hidden">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="./html/quemsomos.html" aria-current="page" data-hover="Quem somos">Quem
+                  somos<span class="visually-hidden">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="./html/contato.html" aria-current="page"
+                  data-hover="Contato">Contato<span class="visually-hidden">(current)</span></a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="./html/produtos.html" id="dropdownId"
+                  data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
+                  data-hover="Produtos">Produtos</a>
+                <div class="dropdown-menu" aria-labelledby="dropdownId">
+                  <a class="dropdown-item" href="./html/bolos.html" data-hover="Contato">Bolos</a>
+                  <a class="dropdown-item" href=".html/doces.html" data-hover="Contato">Doces</a>
+                  <a class="dropdown-item" href="./html/salgados.html" data-hover="Contato">Salgados</a>
+                  <a class="dropdown-item" href="./html/personalizados.html" data-hover="Contato">Personalizados</a>
+                  <a class="dropdown-item" href="./html/kitfesta.html" data-hover="Contato">Kit Festa</a>
+                </div>
+              </li>
+            </ul>
+            <form class="d-flex my-2 my-lg-0">
+              <input class="form-control me-sm-2" type="text" placeholder="O que você procura?">
+              <button class="btn btn-outline my-2 my-sm-0" type="submit"><svg xmlns="http://www.w3.org/2000/svg"
+                  width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                  <path
+                    d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                </svg></button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </div>
+        <br><br>
         <?php
         if(isset($_SESSION['msg'])){
             echo $_SESSION['msg'];
             unset($_SESSION['msg']);
         }
         ?>
-        
+        <br>
+        <h1>Agende o dia da encomenda</h1>
+        <br><br>
         <div id='calendar'></div>
 
         <div class="modal fade" id="visualizar" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -74,7 +149,7 @@ session_start();
                     </div>
                     <div class="modal-body">
                         <span id="msg-cad"></span>
-                        <form id="addevent" method="POST" enctype="multipart/form-data">
+                        <form id="addevent" method="POST" enctype="multipart/form-data" action="">
                             <div class="form-group row">
                                 <label class="col-sm-2 col-form-label">Título</label>
                                 <div class="col-sm-10">
@@ -112,5 +187,7 @@ session_start();
                 </div>
             </div>
         </div>
+        <br><br><br>
+        <?php  include '../footer.php' ?>
     </body>
 </html>
