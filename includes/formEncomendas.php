648a03c380cc3b2461b8218bfaeb9264aab773b4
Melissa Neves <116177595+nevesmelissa@users.noreply.github.com>
2022-12-06 22:03:04 -0300

    encomendas

--- includes/formEncomendas.php
+++ includes/formEncomendas.php
@@ -108,14 +108,5 @@
 
         </div>
 
-        <div class="bg-blue py-4">
-          <div class="row px-3">
-            <small class="ml-4 ml-sm-5 mb-2">Copyright &copy; 2022. Luh Mimu’s.</small>
-            <div class="social-contact ml-4 ml-sm-auto">
-              <span class="fa fa-facebook mr-4 text-sm"></span>
-              <span class="fa fa-instagram mr-4 text-sm"></span>
-              <span class="fa fa-whatsapp mr-4 mr-sm-5 text-sm"></span>
-            </div>
-          </div>
-        </div>
+    
       </div>
